17af199274320441251bd3b9733cee66c610024f
juniverse19 <ryanjkim18@gmail.com>
2025-10-16 15:04:20 +0000

    sadfg;lkmrtnehdglz,fm,ngnklgmds., gn

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+// Ryan Kim
+// 10/16/2025
+// This program determines the perfect pet for the user based on their inputs of favorite color & season and their name
+
 import java.util.Scanner;
 
 public class Main{
@@ -21,13 +25,49 @@ public class Main{
         
         System.out.println("Enter your name");
         String name = scan.nextLine();
+
+        boolean isConsonant = ("qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM".indexOf(name.charAt(0)) != -1);
     
-
-
-
-
-
-
         scan.close();
+
+        //Determines perfect pet
+        String pet = "Placeholder";
+        if (favColor.equals("blue")){
+            if (favSeason.equals("fall")){
+                pet = "alligator";
+            }
+            if (favSeason.equals("spring")){
+                pet = "ostrich";
+            }
+
+        } else if (favColor.equals("green")){
+            if (((isConsonant)) && (favSeason.equals("winter"))) {
+                pet = "giraffe";
+            }
+            if ((!(favSeason.equals("fall"))) && (!(pet.equals("giraffe")))){
+                pet = "dog";
+            }
+        } else {
+            if (!isConsonant){
+                pet = "porcupine";
+            } else {
+                pet = "panda";
+            }
+        }
+
+        if (favSeason.equals("summer") && pet.equals("Placeholder")) {
+            pet = "pony";
+        }
+
+        if (((isConsonant)) && (favColor.equals("blue")) && ((!(favSeason.equals("summer"))) && (!(favSeason.equals("fall")))) && (pet.equals("Placeholder"))) {
+            pet = "axolotl";
+        }
+
+        if (pet.equals("Placeholder")){
+            pet = "rock";
+        }
+
+        System.out.println("Your perfect pet is a " + pet);
+
     }
 }
